20edbc6009e392113771eab74a0375ef51d8bdb1
Julio Eliecer Nova Estrada <142542719+NovaJulio@users.noreply.github.com>
2024-05-09 18:03:30 -0500

    Commit de preparacion

--- src/proyecto2_segundocorte/Proyecto2_SegundoCorte.java
+++ src/proyecto2_segundocorte/Proyecto2_SegundoCorte.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyecto2_segundocorte;
-
-/**
- *
- * @author JulioNova
- */
 public class Proyecto2_SegundoCorte {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
